af16b74343b076a5b1aced0258720682e9ad412c
Aftab Irshad <aftabhive@gmail.com>
2025-04-26 08:32:02 +0100

    Update maintenance-mode.js

--- maintenance-mode.js
+++ maintenance-mode.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const maintenanceMode = {
         enabled: true, // Set to true to enable maintenance mode
         title: "Maintenance in Progress",
-        message: "We are making some changes to our website based on Aftab's feedback, which he has requested or our shortcomings, we will fix it soon, but it will take us some time because I am bringing a transfer function to my HiveCeleo  website, which will show the details of the fund transfer. ",
+        message: "We are making some changes to our website based on dr. Lobes (@drlobes) feedback, which he has requested or our shortcomings, we will fix it soon, but it will take us some time because I am bringing a transfer function to my HiveCeleo  website, which will show the details of the fund transfer. ",
         startTime: "Apr 26, 2025 - 12:31 PM", 
         endTime: "Apr 2025 not Defined Exect Date",
         allowClose: true, // Even if true, popup will reappear on any interaction
